80c98f045e1d9f911c5ca03ff5b431295843b25c
aaron <aaron.martin@iesdoctorbalmis.com>
2020-02-27 19:58:20 +0100

    commit de prueba

--- whatsappgrupo1/src/main/java/es/eoi/grupo1/dto/UsuarioDto.java
+++ whatsappgrupo1/src/main/java/es/eoi/grupo1/dto/UsuarioDto.java
@@ -1,5 +1,20 @@
 package es.eoi.grupo1.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDto {
 
+	int id;
+	
+	String telefono;
+	
+	String mensaje;
+	
 }
