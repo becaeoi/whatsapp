8937f7d886ec579b2a9c864a946a5cfc676c974a
aaron <aaron.martin@iesdoctorbalmis.com>
2020-02-27 18:54:18 +0100

    añado @controller a los repositorios

--- whatsappgrupo1/src/main/java/es/eoi/grupo1/repository/UsuarioRepository.java
+++ whatsappgrupo1/src/main/java/es/eoi/grupo1/repository/UsuarioRepository.java
@@ -1,7 +1,11 @@
 package es.eoi.grupo1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Controller;
 
+import es.eoi.grupo1.entity.Usuario;
+
+@Controller
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
 }
