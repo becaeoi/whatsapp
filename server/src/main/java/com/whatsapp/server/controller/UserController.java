17a437ef28924225cfe038903d30d2d3f4c66c40
manulaiko <manulaiko@gmail.com>
2020-02-28 17:30:49 +0100

    Create register endpoint

--- server/src/main/java/com/whatsapp/server/controller/UserController.java
+++ server/src/main/java/com/whatsapp/server/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.whatsapp.server.controller;
 
+import com.whatsapp.server.dto.UserDto;
 import com.whatsapp.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     private final ModelMapper mapper;
+
+    @PostMapping("register")
+    public UserDto register(@RequestBody UserDto user) {
+        return mapper.map(
+                userService.register(user.getName(), user.getPhone()),
+                UserDto.class
+        );
+    }
 }
--- server/src/main/java/com/whatsapp/server/service/UserService.java
+++ server/src/main/java/com/whatsapp/server/service/UserService.java
@@ -1,8 +1,11 @@
 package com.whatsapp.server.service;
 
+import com.whatsapp.server.entity.User;
 import com.whatsapp.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * User service.
@@ -16,4 +19,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepository repository;
+
+    /**
+     * Registers a new user.
+     *
+     * @param name  User name.
+     * @param phone User phone.
+     *
+     * @return Registered user.
+     */
+    public User register(String name, String phone) {
+        User user = new User();
+        user.setId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE);
+        user.setName(name);
+        user.setPhone(phone);
+
+        return repository.save(user);
+    }
 }
